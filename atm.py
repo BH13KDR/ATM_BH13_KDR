ba0d0ed585ac2d8a66b19f88eeee80480fd4bd85
BH13KDR <bh13kdr@example.com>
2025-07-18 13:24:16 +0900

    ✨ 영수증출력기능 구현
    입출금 내역이 기록되어 영수증으로 출력됩니다. 거래내역이 없으면 관련 정보를 표시합니다.

--- atm.py
+++ atm.py
@@ -3,6 +3,7 @@
 #기능 입력을 받는 기능을 구현.
 
 balance = 300000
+receipts = []
 
 while True : #아래에 무슨 조건이 오던 무한히 실행.
         num = input("사용하실 기능의 번호를 입력해주세요. (1.입금, 2.출금, 3.영수증 보기, 4.종료) :")
@@ -12,13 +13,25 @@ while True : #아래에 무슨 조건이 오던 무한히 실행.
             deposit_amount = int(input("입금할 금액을 입력해주세요."))
             balance += deposit_amount
             print(f'입금하신 금액{deposit_amount}원. 현재잔액은 {balance}입니다.')
+            receipts.append(("입금",deposit_amount,balance))
+            #입력한 입금 금액을 튜플의 형태로 receipts리스트 안에 튜플로 저장한다.
+
         if num == '2':
             withdraw_amount = int(input("출금할 금액을 입력해주세요."))
             withdraw_amount = min(balance,withdraw_amount)
             balance -= withdraw_amount
             print(f'출금하신 금액{withdraw_amount}원. 현재잔액은 {balance}입니다.')
+            receipts.append(("출금",withdraw_amount,balance))
+
         if num == '3':
-            pass #임시구현
+            if receipts: #라고만 적어도 컴퓨터는 값에 접근해 값이 있는지T 없는지F판단.
+                  #print(receipts) 으로 해도 되지만 예쁘게
+                  for i in receipts:
+                        print(f"{i[0]} : {i[1]}원 : 잔액 : {i[2]}원")
+                        # i = ('입금', 3000, 303000)
+                        # i[0] = '입금'
+            else :
+                  print("거래내역이 없습니다.")
         
 
 print(f'서비스를 종료합니다. 현재 잔액 : {balance}')
